2512d339954da29cabe644320621d31c124b7034
Marina SINAMA-PONGOLLE <msinamapongolle@ippon.fr>
2017-10-03 16:42:56 +0200

    add form component

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Item } from './item';
-import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { ModalComponent } from './modal/modal.component';
 
 @Component({
   selector: 'app-root',
@@ -13,32 +10,6 @@ export class AppComponent implements OnInit {
 
   collection: Item[];
 
-  form: FormGroup;
-
-  nameCtrl: FormControl;
-
-  refCtrl: FormControl;
-
-  stateCtrl: FormControl;
-
-  constructor (fb: FormBuilder, private modalService: NgbModal) {
-
-    this.nameCtrl = fb.control('', [
-                                  Validators.required,
-                                  Validators.minLength(2),
-                                  Validators.pattern('')]);
-    this.refCtrl = fb.control('', [
-                                  Validators.required,
-                                  Validators.minLength(4)]);
-    this.stateCtrl = fb.control(0);
-
-    this.form = fb.group({
-      name: this.nameCtrl,
-      reference: this.refCtrl,
-      state: this.stateCtrl
-    });
-  }
-
   ngOnInit(): void {
     this.collection = [
       new Item({reference: '1234', name: 'Marina', state: 0}),
@@ -48,32 +19,8 @@ export class AppComponent implements OnInit {
 
   }
 
-  addItem() {
-    console.log(this.form.value);
-    // si les names du form sont différents du model (exemple ref dans form et reference côté model)
-    // this.collection.push({
-    //  name:this.form.get('name').value,
-    //  reference:this.form.get('ref').value,
-    //  state:this.form.get('state').value,});
-    this.collection.push(this.form.value);
-    this.reset();
-    this.open();
-
+  addItem(item: Item) {
+    this.collection.push(item);
   }
-
-  reset() {
-    this.form.reset();
-    this.stateCtrl.setValue(0);
-  }
-
-  isError (field: string) {
-    return this.form.get(field).dirty && this.form.get(field).hasError('minlength');
-  }
-
-  open() {
-    const modalRef = this.modalService.open(ModalComponent);
-    modalRef.componentInstance.msg = 'Votre commande a été enregistrée!';
-  }
-
 
 }
